8e0207da28a5ff6760843381739f0ed0dc4befa0
Matheus Solha <mcsolha@gmail.com>
2018-10-04 17:32:04 -0300

    code to read all sensors

---
+++ all_in_one.ino
@@ -0,0 +1,66 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+#include "DHT.h"
+
+#define DHTPIN 13       // what digital pin DHT is connected to
+#define LDRPIN 4        // what digital pin LDR is connected to
+#define REEDSWITCHPIN 2 // what digital pin REED SWITCH is connected to
+
+#define DHTTYPE DHT11   // DHT 11
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(REEDSWITCHPIN, INPUT_PULLUP);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  int ldr_state = digitalRead(LDRPIN);
+  int reed_state = digitalRead(REEDSWITCHPIN);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+  
+  Serial.print("LDR:  ");
+  Serial.println(ldr_state);
+  
+  Serial.print("REED SWITCH:  ");
+  Serial.println(reed_state);
+}
